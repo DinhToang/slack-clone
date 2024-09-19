35c768ac6c53964646ab19a8fbaf53a8a132270f
Ashe <toannguyen03.arc@gmail.com>
2024-09-19 23:39:33 +0700

    add auth screen

---
+++ src/feartures/auth/components/auth-screen.tsx
@@ -0,0 +1,9 @@
+"use clent";//this is a react component not a react server component
+
+export const AuthScreen = () =>{
+    return(
+        <div>
+            Auth screen
+        </div>
+    )
+}
