06833b8ae65037fa5e03c20372f072e547636be7
Ashe <toannguyen03.arc@gmail.com>
2024-09-20 09:25:42 +0700

    Add comment

--- src/feartures/auth/components/auth-screen.tsx
+++ src/feartures/auth/components/auth-screen.tsx
@@ -1,4 +1,4 @@
-"use clent";//this is a react component not a react server component
+"use clent";//this is a react component not a react server component, can use reactivity and interactivity inside it: use state or on click 
 
 export const AuthScreen = () =>{
     return(
